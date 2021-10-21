f70cec898e1e7db932a0478c814d740bffc4b45b
Huan Wang <huanw@zju.edu.cn>
2021-10-21 16:42:51 -0400

    updating

--- smilelogging/__init__.py
+++ smilelogging/__init__.py
@@ -1,4 +1,4 @@
-from researchlogging.logger import Logger
+from smilelogging.logger import Logger
 import configargparse
 argparser = configargparse.ArgumentParser()
 argparser.add_argument('--project_name', type=str, default='')
@@ -6,4 +6,3 @@ argparser.add_argument('--debug', action="store_true")
 argparser.add_argument('--screen_print', action="store_true")
 argparser.add_argument('--cache_ignore', type=str, default='')
 argparser.add_argument('--note', type=str, default='')
-argparser.add_argument('--CodeID', type=str, default='')
