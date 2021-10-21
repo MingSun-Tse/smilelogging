856c6aa00b57fea8d9460e72b627903711690659
Huan Wang <huanw@zju.edu.cn>
2021-10-21 13:44:56 -0400

    updating

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'researchlogging',         # How you named your package folder (MyLib)
   packages = ['researchlogging'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Huan Wang',                   # Type in your name
