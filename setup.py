24acac34c2502b1dd0c940d686fb0dbe10d92af8
Huan Wang <huanw@zju.edu.cn>
2021-10-21 01:09:20 -0400

    updating

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'ezlogging',         # How you named your package folder (MyLib)
+  packages = ['ezlogging'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'Huan Wang',                   # Type in your name
+  author_email = 'huan.wang.cool@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/MingSun-Tse/ezlogging/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
+  keywords = ['logging', 'reproducible experimenting'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+          'matplotlib', 
+          'Pillow',
+          'PyYAML',
+          'lmdb',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
