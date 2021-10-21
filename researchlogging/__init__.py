7d7a38e287b01062bb7b1c37a8edbada17270b69
Huan Wang <huanw@zju.edu.cn>
2021-10-21 13:44:56 -0400

    updating

--- researchlogging/__init__.py
+++ researchlogging/__init__.py
@@ -1,1 +1,8 @@
 from researchlogging.logger import Logger
+import configargparse
+argparser = configargparse.ArgumentParser()
+argparser.add_argument('--project_name', type=str, default="")
+argparser.add_argument('--debug', action="store_true")
+argparser.add_argument('--screen_print', action="store_true")
+argparser.add_argument('--cache_ignore', type=str, default='')
+argparser.add_argument('--note', type=str, default='')
