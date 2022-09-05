12c71b57ea3d4f520bc715cacb936e8ce0e7a69e
Huan Wang <wang.huan@northeastern.edu>
2022-09-05 10:03:28 -0400

    Update script

--- scripts/sync_git.sh
+++ scripts/sync_git.sh
@@ -1,4 +1,4 @@
-git pull origin138 master
+sshpass -p $1 git pull origin138 master
 git pull origin master
-git push origin138 master
+sshpass -p $1 git push origin138 master
 git push origin master
