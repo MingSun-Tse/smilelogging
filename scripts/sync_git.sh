3f73e353bfc6f65c06a640bf1bb42a15aec70347
Huan Wang <huanw@zju.edu.cn>
2022-09-05 15:07:06 -0400

    Update

--- scripts/sync_git.sh
+++ scripts/sync_git.sh
@@ -1,11 +1,9 @@
 
-git_change=$(git diff --name-only)
-if [ $git_change ]; then
-    git add -u .
-    read -p "git commit -m " gcm
-    echo $gcm
-    git commit -m $gcm
-fi
+
+git add -u .
+read -p "git commit -m " gcm
+git commit -m $gcm
+
 sshpass -p $1 git pull origin138 master
 git pull origin master
 sshpass -p $1 git push origin138 master
