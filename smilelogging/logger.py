a101f7bfca102509a2d99c4a45778c6ca4f884ff
Huan Wang <wang.huan@northeastern.edu>
2023-01-28 00:11:33 -0500

    Update

--- smilelogging/logger.py
+++ smilelogging/logger.py
@@ -8,21 +8,27 @@ import yaml
 import builtins
 import traceback
 from fnmatch import fnmatch
+
 pjoin = os.path.join
+
 
 class DoubleWriter():
     def __init__(self, f1, f2):
         self.f1, self.f2 = f1, f2
+
     def write(self, msg):
         self.f1.write(msg)
         self.f2.write(msg)
+
     def flush(self):
         self.f1.flush()
         self.f2.flush()
 
+
 class LogTracker():
     r"""Logging all numerical results.
     """
+
     def __init__(self):
         self._metrics = OrderedDict()
         self._print_format = {}
@@ -38,16 +44,16 @@ class LogTracker():
             self._metrics[k] = np.append(self._metrics[k], v)
         else:
             self._metrics[k] = np.array([v])
-    
+
     def reset(self):
         self._metrics = OrderedDict()
-    
+
     def get_metrics(self, k=None):
         if k is not None:
             return self._metrics[k]
         else:
             return self._metrics
-    
+
     def format(self, selected=None, not_selected=None, sep=' '):
         r"""Format for print.
         """
@@ -61,7 +67,7 @@ class LogTracker():
                     if fnmatch(k, s):
                         in_selected = True
                         break
-            
+
             if not_selected is not None:
                 for s in not_selected.split(','):
                     if fnmatch(k, s):
@@ -70,17 +76,18 @@ class LogTracker():
 
             if in_selected:
                 f = self._print_format[k] if k in self._print_format else f'%s'
-                logstr += [ f'{k} {f}' % v[-1] ]
+                logstr += [f'{k} {f}' % v[-1]]
         return sep.join(logstr)
-    
+
     def get_ma(self, k, window=10):
         r"""Moving average.
         """
         return moving_average(self._metrics[k], window)
 
+
 class Logger(object):
     passer = {}
-    
+
     def __init__(self, args):
         self.args = args
         self.sl_cfg = '.smilelogging.cfg'
@@ -91,7 +98,7 @@ class Logger(object):
         self._weights_dir = 'weights'
         self._gen_img_dir = 'gen_img'
         self._log_dir = 'log'
-        
+
         # customize logging folder names
         if hasattr(args, 'hacksmile') and args.hacksmile.config:
             for line in open(args.hacksmile.config):
@@ -101,7 +108,7 @@ class Logger(object):
                 else:
                     attr, value = [x.strip() for x in line.split(':')]
                     self.__setattr__(attr, value)
-        
+
         # Set up a unique experiment folder
         self.userip, self.hostname = self.get_userip()
         self.ExpID = self.get_ExpID()
@@ -114,15 +121,15 @@ class Logger(object):
 
         # Initial print
         self.print_script()
-        args.CodeID = self.get_CodeID() # get CodeID before 'print_args()' because it will be printed in log
+        args.CodeID = self.get_CodeID()  # get CodeID before 'print_args()' because it will be printed in log
         self.print_args()
 
         # Cache misc environment info  (e.g., GPU, git, etc.) and code files
-        self.save_args() # to .yaml
-        self.save_nvidia_smi() # print GPU info
+        self.save_args()  # to .yaml
+        self.save_nvidia_smi()  # print GPU info
         self.save_git_status()
         self.cache_done = False
-        self.cache_model() # backup code
+        self.cache_model()  # backup code
         self.n_log_item = 0
 
     def get_CodeID(self):
@@ -130,14 +137,15 @@ class Logger(object):
         """
         git_check = run_shell_command('git rev-parse --is-inside-work-tree')
         self.use_git = len(git_check) == 1 and git_check[0] == 'true'
-        self.git_root = run_shell_command('git rev-parse --show-toplevel')[0] # The path where .git is placed
+        self.git_root = run_shell_command('git rev-parse --show-toplevel')[0]  # The path where .git is placed
 
         if self.use_git:
             CodeID = run_shell_command('git rev-parse --short HEAD')
             changed_files = run_shell_command('git diff --name-only')
             new_real_change = False
             for f in changed_files:
-                if f and not f.endswith('.sh') and f.endswith('.slurm'): # Not consider shell script change as a formal change
+                if f and not f.endswith('.sh') and f.endswith(
+                        '.slurm'):  # Not consider shell script change as a formal change
                     new_real_change = True
             if new_real_change:
                 logtmp = "Warning! There is (at least) one uncommitted change in your code. Recommended to git commit it first"
@@ -153,7 +161,7 @@ class Logger(object):
         r"""Assign a unique experiment id for the current experiment
         """
         self.SERVER = '%03d' % int(self.userip.split('.')[-1])
-        
+
         if hasattr(self.args, 'resume_ExpID') and self.args.resume_ExpID:
             project_path = get_project_path(self.args.resume_ExpID)
             ExpID = parse_ExpID(project_path)
@@ -167,25 +175,25 @@ class Logger(object):
         project_path = pjoin("%s/%s_%s" % (self._experiments_dir, self.args.project_name, self.ExpID))
         if hasattr(self.args, 'resume_ExpID') and self.args.resume_ExpID:
             project_path = get_project_path(self.args.resume_ExpID)
-        if self.args.debug: # debug has the highest priority. If debug, all the things will be saved in Debug_dir
+        if self.args.debug:  # debug has the highest priority. If debug, all the things will be saved in Debug_dir
             project_path = self._debug_dir
 
         # Output interface
-        self.exp_path     = project_path
+        self.exp_path = project_path
         self.weights_path = pjoin(project_path, self._weights_dir)
         self.gen_img_path = pjoin(project_path, self._gen_img_dir)
-        self.log_path     = pjoin(project_path, self._log_dir)
-        self.logplt_path  = pjoin(self.log_path, "plot")
-        self.logtxt_path  = pjoin(self.log_path, "log.txt")
-        self._cache_path  = pjoin(project_path, ".caches")
-        mkdirs(self.weights_path, self.gen_img_path, self.logplt_path, self._cache_path)         
+        self.log_path = pjoin(project_path, self._log_dir)
+        self.logplt_path = pjoin(self.log_path, "plot")
+        self.logtxt_path = pjoin(self.log_path, "log.txt")
+        self._cache_path = pjoin(project_path, ".caches")
+        mkdirs(self.weights_path, self.gen_img_path, self.logplt_path, self._cache_path)
         self.logtxt = open(self.logtxt_path, "a+")
 
         # user can customize the folders in experiment dir
         if hasattr(self.args, 'hacksmile') and self.args.hacksmile.config:
             for line in open(self.args.hacksmile.config):
                 line = line.strip()
-                if line.startswith('!reserve_dir'): # example: !reserve_dir: misc_results
+                if line.startswith('!reserve_dir'):  # example: !reserve_dir: misc_results
                     assert len(line.split(':')) == 2, f"There should be only one ':' in the line. Please check: f{line}"
                     dir_name = line.split(':')[1].strip()
                     dir_path = f'{self.exp_path}/{dir_name}'
@@ -204,14 +212,14 @@ class Logger(object):
         if os.path.isfile('.cache_ignore'):
             for line in open('.cache_ignore'):
                 ignore_from_file += line.strip().split(',')
-        
+
         ignore_from_arg = []
         if hasattr(self.args, 'cache_ignore') and self.args.cache_ignore:
             ignore_from_arg += self.args.cache_ignore.split(',')
-        
+
         ignore = ignore_default + ignore_from_file + ignore_from_arg
-        ignore = list(set(ignore)) # Remove repeated items
-        
+        ignore = list(set(ignore))  # Remove repeated items
+
         # If there is new cache_ignore, save it
         if set(ignore) != set(ignore_from_file):
             with open('.cache_ignore', 'w') as f:
@@ -233,7 +241,7 @@ class Logger(object):
         level = str(level).lower()
         assert level in ['0', '10', '20', '30', '40', '50',
                          'notset', 'debug', 'info', 'warning', 'error', 'critical']
-                # See https://docs.python.org/3/library/logging.html#levels
+        # See https://docs.python.org/3/library/logging.html#levels
         if level in ['0', 'notset']:
             info = '\b'
         elif level in ['10', 'debug']:
@@ -250,11 +258,16 @@ class Logger(object):
         # Get the final message to print
         msg = sep.join([str(m) for m in msg]) + end
         if acc:
-            msg = '  ' * int(self.ExpID[-1]) + msg
-        prefix = '' if unprefix else "[%s %s %s] [%s:%d] %s " % (self.ExpID[-6:], os.getpid(),
-                                time.strftime("%Y/%m/%d-%H:%M:%S"),
-                                filename, lineno, info)
-        msg = prefix + msg
+            msg = '  ' * int(self.ExpID[-1]) + msg  # Add blanks to acc lines for easier identification
+
+        if not unprefix:
+            prefix = "[%s %s %s] [%s:%d] %s " % (self.ExpID[-6:],
+                                                 os.getpid(),
+                                                 time.strftime("%Y/%m/%d-%H:%M:%S"),
+                                                 filename,
+                                                 lineno,
+                                                 info)
+            msg = prefix + msg
 
         # Print
         if file is None:
@@ -349,7 +362,7 @@ class Logger(object):
                 key_map[k_lower + '_' + k_lower] = k
             else:
                 key_map[k_lower] = k
-        
+
         # print in the order of sorted lower keys 
         logtmp = ''
         for k_ in sorted(key_map.keys()):
@@ -361,12 +374,12 @@ class Logger(object):
         out = pjoin(self.log_path, 'gpu_info.txt')
         script = 'nvidia-smi >> %s' % out
         os.system(script)
-    
+
     def save_git_status(self):
         if self.use_git:
             script = 'git status >> %s' % pjoin(self.log_path, 'git_status.txt')
             os.system(script)
-    
+
     def plot(self, name, out_path):
         self.log_tracker.plot(name, out_path)
 
@@ -374,14 +387,15 @@ class Logger(object):
         r"""Save the modle architecture, loss, configs, in case of future check.
         """
         if self.args.debug or self.cache_done: return
-        
+
         t0 = time.time()
         if not os.path.exists(self._cache_path):
             os.makedirs(self._cache_path, exist_ok=True)
         logtmp = f"==> Caching various config files to '{self._cache_path}'"
         self.print(logtmp)
-        
-        extensions = ['.py', '.json', '.yaml', '.sh', '.txt', '.md'] # files of these types will be cached
+
+        extensions = ['.py', '.json', '.yaml', '.sh', '.txt', '.md']  # files of these types will be cached
+
         def copy_folder(folder_path):
             for root, _, files in os.walk(folder_path):
                 if '__pycache__' in root: continue
@@ -394,9 +408,10 @@ class Logger(object):
                             os.makedirs(dir_path, exist_ok=True)
                         if os.path.exists(f_path):
                             sh.copy(f_path, dir_path)
-        
+
         # copy files in current dir
-        [sh.copy(f, self._cache_path) for f in os.listdir('.') if os.path.isfile(f) and os.path.splitext(f)[1] in extensions]
+        [sh.copy(f, self._cache_path) for f in os.listdir('.') if
+         os.path.isfile(f) and os.path.splitext(f)[1] in extensions]
 
         # copy dirs in current dir
         [copy_folder(d) for d in os.listdir('.') if os.path.isdir(d) and d not in self.cache_ignore]
@@ -408,11 +423,11 @@ class Logger(object):
         ''' For example, 'Projects/FasterRCNN/logger.py', then return 'FasterRCNN' '''
         file_path = os.path.abspath(__file__)
         return file_path.split('/')[-2]
-    
+
     def save_args(self):
         with open(pjoin(self.log_path, 'args.yaml'), 'w') as f:
             yaml.dump(self.args.__dict__, f, indent=4)
-    
+
     def netprint(self, net, comment=''):
         r"""Deprecated. Will be removed.
         """
