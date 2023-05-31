9adcfc164a424fe9e4f0292d4e2b325702249cf1
Huan Wang <wang.huan@northeastern.edu>
2023-05-30 21:55:11 -0500

    "Update"

--- smilelogging/__init__.py
+++ smilelogging/__init__.py
@@ -1,19 +1,13 @@
-from smilelogging.logger import Logger
-from smilelogging.utils import update_args
 import configargparse
 import functools
-<<<<<<< HEAD
-
-argparser = configargparse.ArgumentParser()
-argparser.add_argument('--project_name',
-                       '--experiment_name',
-                       dest='project_name',
-=======
 import sys
+
+from smilelogging.logger import Logger
+from smilelogging.utils import update_args
+
 
 argparser = configargparse.ArgumentParser()
 argparser.add_argument('--experiment_name',
->>>>>>> 6f780b94b87c19e447577e819416a7def237d0e1
                        type=str,
                        default='',
                        help='experiment name')
@@ -31,12 +25,9 @@ argparser.add_argument('--cache_code',
                        type=str,
                        default='scripts/cache_code.sh',
                        help='the script to cache code')
-<<<<<<< HEAD
-=======
 argparser.add_argument('--no_scp',
                        action='store_true',
                        help='not scp experiment to hub')
->>>>>>> 6f780b94b87c19e447577e819416a7def237d0e1
 argparser.add_argument('--resume_TimeID',
                        type=str,
                        default='',
@@ -47,22 +38,7 @@ argparser.add_argument('--sl.ON',
                        action='store_true')
 argparser.add_argument('--sl.config',
                        type=str,
-<<<<<<< HEAD
                        default='.smilelogging_cfg')
-
-
-def print_runtime(fn):
-    r"""Print the running time of a routine.
-    """
-    @functools.wraps(fn)
-    def wrapper(*args, **kw):
-        ret = fn(*args, **kw)
-        print(f'( "{fn.__name__}" executed in {t1 - t0:.4f}s )')
-        return ret
-    return wrapper
-=======
-                       default='.smilelogging_cfg',
-                       help='smilelogging config file, yaml, used for customizing smilelogging')
 
 def warn_deprecated_args(old, new):
     if old in sys.argv:
@@ -80,6 +56,3 @@ def add_update_args(fn):
 warn_deprecated_args('--project_name', '--experiment_name')
 warn_deprecated_args('--project', '--experiment_name')
 argparser.parse_args = add_update_args(argparser.parse_args)
-
-
->>>>>>> 6f780b94b87c19e447577e819416a7def237d0e1
